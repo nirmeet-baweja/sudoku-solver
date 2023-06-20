eb0ba8562c099036cc177c35c860abcb4e751a9d
nirmeet-baweja <nirmeet.baweja@gmail.com>
2023-06-20 23:26:57 +0100

    setting up algorithm file

---
+++ src/utilities/algorithm.js
@@ -0,0 +1,17 @@
+const solver = (grid, currentRow, currentCol) => {
+  const initObject = {
+    0: [],
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+  };
+
+  const rowMap = new Map();
+  const colMap = new Map();
+  const gridMap = new Map();
+};
