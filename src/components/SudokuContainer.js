37ec9268f600e7daa70b4ddf298c4a971350e72e
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-06-25 20:42:41 +0100

    clear sudoku functionality

--- src/components/SudokuContainer.js
+++ src/components/SudokuContainer.js
@@ -10,15 +10,31 @@ const SudokuContainer = ({ grid, setGrid }) => {
     setGrid(copyGrid);
   };
 
+  const clearSudoku = () => {
+    const rows = 9;
+    const columns = 9;
+    const rowTemplate = Array(columns).fill("");
+    const emptyGrid = Array.from(Array(rows), () => [...rowTemplate]);
+    setGrid(emptyGrid);
+  };
+
   return (
     <div className="flex flex-col justify-center max-w-4xl m-auto content-center">
       <Sudoku grid={grid} setGrid={setGrid} />
-      <button
-        className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded w-min m-auto my-4"
-        onClick={solveSudoku}
-      >
-        Solve
-      </button>
+      <div className="inline-flex justify-center">
+        <button
+          className="bg-teal-600 hover:bg-teal-500 text-white font-bold py-2 px-4 rounded w-min m-4"
+          onClick={solveSudoku}
+        >
+          Solve
+        </button>
+        <button
+          className="bg-pink-600 hover:bg-pink-500 text-white font-bold py-2 px-4 rounded w-min m-4"
+          onClick={clearSudoku}
+        >
+          Clear
+        </button>
+      </div>
     </div>
   );
 };
