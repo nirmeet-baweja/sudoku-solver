36c0a99d73fe1aad16c81b5fd1984eac16c4d47c
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-06-25 16:37:32 +0100

    updated SudokuCell to handle input validation

--- src/components/SudokuCell.js
+++ src/components/SudokuCell.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { deepCopy } from "../utilities/helper";
 
-const SudokuCell = ({ num, grid, row, col, setGrid }) => {
-  // const [number, setNumber] = useState(num);
-
+const SudokuCell = ({ grid, row, col, setGrid }) => {
   const handleChange = (event) => {
-    // setNumber(event.target.value);
+    const inputNum = Number(event.target.value);
     const newGrid = deepCopy(grid);
-    newGrid[row][col] = event.target.value;
-    console.log(newGrid);
+    if (inputNum > 0 && inputNum < 10) {
+      newGrid[row][col] = inputNum;
+    } else {
+      newGrid[row][col] = "";
+    }
     setGrid(newGrid);
   };
 
   return (
     <td className="border  border-slate-400 w-12 h-12">
-      <input value={grid[row][col]} onChange={handleChange} className="w-full"></input>
+      <input
+        value={grid[row][col]}
+        onChange={handleChange}
+        className="w-full h-full text-center"
+      ></input>
     </td>
   );
 };
