1855bd269d496e33534a955d93a9c509e4120637
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2023-06-26 17:42:05 +0100

    simplified sudoku cell style update logic

--- src/components/SudokuCell.js
+++ src/components/SudokuCell.js
@@ -11,6 +11,26 @@ const SudokuCell = ({ grid, row, col, setGrid }) => {
     "focus:outline-pink-500",
   ]);
 
+  const userInputClassList = [
+    "w-full",
+    "h-full",
+    "text-center",
+    "text-pink-500",
+    "font-bold",
+    "bg-slate-100",
+    "cursor-pointer",
+    "focus:outline-pink-500",
+  ];
+
+  const emptyInputClassList = [
+    "w-full",
+    "h-full",
+    "text-center",
+    "text-slate-700",
+    "cursor-pointer",
+    "focus:outline-pink-500",
+  ];
+
   useEffect(() => {
     if (grid[row][col] === "") {
       setClassList([
@@ -29,24 +49,10 @@ const SudokuCell = ({ grid, row, col, setGrid }) => {
     const newGrid = deepCopy(grid);
     if (inputNum > 0 && inputNum < 10) {
       newGrid[row][col] = inputNum;
-      const newClassList = classList.map((classItem) =>
-        classItem === "text-slate-700" ? "text-pink-500" : classItem
-      );
-      !newClassList.includes("font-bold") && newClassList.push("font-bold");
-      !newClassList.includes("bg-slate-100") &&
-        newClassList.push("bg-slate-100");
-
-      setClassList(newClassList);
+      setClassList(userInputClassList);
     } else {
       newGrid[row][col] = "";
-      const newClassList = classList.filter(
-        (classItem) => classItem !== "font-bold" && classItem !== "bg-slate-100"
-      );
-      const index = newClassList.indexOf("text-pink-500");
-      if (index !== -1) {
-        newClassList[index] = "text-slate-700";
-      }
-      setClassList(newClassList);
+      setClassList(emptyInputClassList);
     }
     setGrid(newGrid);
   };
